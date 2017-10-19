14feaece7c09612a01108ecc624111b1560774ce
walunjsachin <walunj_sachin@msn.com>
2017-10-18 22:10:22 -0400

    Update TestForm.aspx.cs

--- myFirstAzureWebApp/Views/TestForm.aspx.cs
+++ myFirstAzureWebApp/Views/TestForm.aspx.cs
@@ -12,7 +12,7 @@ namespace myFirstAzureWebApp.Views
         protected void Page_Load(object sender, EventArgs e)
         {
             //test forms another comment
-            //onre more line comments
+            //onre more line comments 123
         }
     }
 }
