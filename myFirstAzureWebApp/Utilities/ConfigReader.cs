29525d09ad418d17b90c0f192d20b64f9bf19085
walunjsachin <walunj_sachin@msn.com>
2018-04-11 13:21:31 -0400

    adding configreader.cs file

---
+++ myFirstAzureWebApp/Utilities/ConfigReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace myFirstAzureWebApp.Utilities
+{
+    public static class ConfigReader
+    {
+        public static string GetApplicationString(string keyName)
+        {
+            return ConfigurationManager.AppSettings[keyName];
+        }
+    }
+}
